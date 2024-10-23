aceccceda638c810716003cf0292144030671a0c
Devjohn07 <joaogamer6516@gmail.com>
2024-10-23 14:33:06 -0300

    Update calculadora.py
    tem como calcular um número elevado a outro agr kk

--- calculadora.py
+++ calculadora.py
@@ -1,31 +1,33 @@
-
+# Menu da calculadora
 print ("---------------------------------------------------------------------------------------------------------------------------")
 print("\t \t \t \t \t \t CALCULADORA TEXTUAL")
 print ("---------------------------------------------------------------------------------------------------------------------------")
 print("\n")
-operacao = input("digite qual operação deseja usar(digite exatamente como mostrado a seguir): \n + \n - \n * \n /\n \n")
+operacao = input("digite qual operação deseja usar(digite exatamente como ilustrado a seguir): \nsoma: + \nsubtração: - \nmultiplicação: * \ndivisão: /\nelevado a: **\n \n")
 print ("\n")
-x = int (input("Ok, digite o primeiro numero da operação: ")) 
-y = int(input("o segundo numero: "))
-print("\n")
 
-if operacao == "+":
-    print("PERFEITO, seu calculo está aqui:\n")
-    print(f"{x} + {y} = {x + y}")
+#
+if operacao not in ["+", "-", "*", "/", "**"]:
+    print("---------------------------------------------------------------------------------------------------------------------------")
+    print("\t \t \t \t \t \t OPERAÇÃO INVALIDA, TENTE NOVAMENTE!")
+    print("---------------------------------------------------------------------------------------------------------------------------")
 
-if operacao == "-":
-    print("PERFEITO, seu calculo está aqui:\n")
-    print(f"{x} - {y} = {x - y}")
-
-if operacao == "*":
-    print("PERFEITO, seu calculo está aqui:\n")
-    print(f"{x} * {y} = {x * y}")
-
-if operacao == "/":
-    print("PERFEITO, seu calculo está aqui:\n")
-    print(f"{x} / {y} = {x / y}")
-
-
-
-
-
+#solicita os valores    
+else:
+     x = int (input("Ok, digite o primeiro numero da operação: ")) 
+     y = int(input("o segundo numero: "))
+     if operacao == "+":
+        print("PERFEITO, seu calculo está aqui:\n")
+        print(f"{x} + {y} = {x + y}")
+     if operacao == "-":
+        print("PERFEITO, seu calculo está aqui:\n")
+        print(f"{x} - {y} = {x - y}")
+     if operacao == "*":
+        print("PERFEITO, seu calculo está aqui:\n")
+        print(f"{x} * {y} = {x * y}")
+     if operacao == "/":
+        print("PERFEITO, seu calculo está aqui:\n")
+        print(f"{x} / {y} = {x / y}")
+     if operacao == "**":
+        print("PERFEITO, seu calculo está aqui:\n")
+        print(f"{x} ** {y} = {x ** y}")
