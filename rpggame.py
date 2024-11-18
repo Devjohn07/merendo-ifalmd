55e65e039a8473a74b94480fdde26de170c7caf4
Devjohn07 <joaogamer6516@gmail.com>
2024-11-18 15:37:53 -0300

    Update rpggame.py

--- rpggame.py
+++ rpggame.py
@@ -12,6 +12,7 @@ def girarD20():
    valor_aleatorio = random.randint(1, 20)
    print(f'\n"um dado foi rolado para testar a sua sorte"\n')
 
+#Escolhe um atributo para ser usado  
 def usar_atributo(valor):
    atributo = input("Qual atributo deseja usar?\nVITALIDADE: Reduz o dano recebido\nFORÇA: Adiciona dano a armas corpo-a-corpo, porém, diminui a chance de acerto\nreação: Dependendo, você pode desviar do ataque eminente, porém existe a chance de n desviar e receber dano completo\n")
    if atributo == "resistencia":
@@ -19,11 +20,13 @@ def usar_atributo(valor):
       print(f"Vou girar um d20+{resistencia}") 
       print(f"você tirou {total} no dado!")
 
+#imprime o inventário no menu
 def imprimir_inventario(inventário):
     print("Inventário:")
     for item, quantidade in inventário.items():
         print(f"{item}: {quantidade}")
 
+#Verifica a quantidade de intens no inventário(usado principalmente para que o usuário sempre pegue uma quantidade especifica de itens nas cenas)
 def verificar_quantidade(inventário, item):
     if item in inventário and inventário[item] >= 1:
         return True
@@ -94,6 +97,7 @@ def menu():
             resistencia += pontosgastos
             pontos -= pontosgastos
             print(f"OK, pontos foram gastos")
+
       #inspeciona os itens que tem no inventário
       #números dado para cada item no jogo: faca[1], pistola[2], munição[3], roupas_limpas[4]
       if optionsmenu == 2:
@@ -115,14 +119,16 @@ def menu():
                   print("roupas limpas, elas possuem uma defesa razoável e são definitivamente seguras(+5 vitalidade)")
          else:
             print("\nSeu inventário está vazio\n")
+
       #faz um tutorial para explicar as funcionalidades do menu
       if optionsmenu == 3:
-         print("\nATRIBUTOS: São seus status, oque define sua vida, sua força, seu nivel de reação\n\tVITALIDADE: Defini sua vida, se ela zerar, acabou\n\tFORÇA: define o quão forte você é e ajuda em testes que exijam força fisica\n\tREAÇÃO: define sua velocidade de reação, usada em testes que peça ao personagem para desviar de algo\n\t RESISTÊNCIA: Ajuda em testes que exijam resistência(tanto fisica quanto mentaL)\n\tARCANO: melhora sua sorte para achar itens e também aumenta dano de armas elementais\n")
+         print("\nATRIBUTOS: São seus status, oque define sua vida, sua força, seu nivel de reação\n\tVITALIDADE: Defini sua vida, se ela zerar, acabou\n\tFORÇA: define o quão forte você é e ajuda em testes que exijam força fisica\n\tREAÇÃO: define sua velocidade de reação, usada em testes que peça ao personagem para desviar de algo\n\tRESISTÊNCIA: Ajuda em testes que exijam resistência(tanto fisica quanto mentaL)\n\tARCANO: melhora sua sorte para achar itens e também aumenta dano de armas elementais\n")
          print("INVENTÁRIO: Usado para mostrar quais itens você possui guardado\n")
+         
       #simplesmente sai do menu
       if optionsmenu == 4:
          break
-#titulo do jogo
+#titulo do jogo e começo da primeira cena
 print ("---------------------------------------------------------------------------------------------------------------------------")
 print("""              _____          ____          _   _          
      /\      / ____|   /\   |  _ \   /\   | \ | |   /\    
@@ -134,7 +140,7 @@ print("""              _____          ____          _   _
                                                            """)
 print ("---------------------------------------------------------------------------------------------------------------------------")
 print("jogo feito pelo Paulino ;)")
-print("comandos:\ncontinuar\ninteragir\natacar\nmenu\nusar atributo\ncomandos")
+print("comandos:\ncontinuar\ninteragir\natacar\nmenu\ncomandos")
 print("\n\n")
 time.sleep(2)
 print("""       _.-'''-._
@@ -150,8 +156,8 @@ print("""       _.-'''-._
 print("Você acorda com o brilho da lua em sua face, praticamente cegando-te, A única coisa visivel é uma cortina branca")
 resposta1 = input("O que quer fazer?\n")
 
-#Acaba de acordar, opções da primeira cena
-while resposta1 not in ["menu", "interagir", "atacar", "continuar", "comandos", "usar atributo" ]:
+#Acaba de acordar
+while resposta1 not in ["menu", "interagir", "atacar", "continuar", "comandos"]:
     print("comando não identificado, tente novamente")
     resposta1 = input("O que quer fazer?\n")
 
@@ -166,13 +172,10 @@ while resposta1 not in "continuar":
         print("\nvocê fechou a cortina, agr você enxerga melhor\n")
         interagir = 1
 
-    elif resposta1 == "usar atributo":
-       print("\nnenhum atributo pode ser usado aqui\n")
-
     elif resposta1 == "comandos":
        print("\nAqui estão todos os comandos possiveis:\ncontinuar\ninteragir\natacar\nmenu\nusar atributo\ncomandos\n")
 
-    elif resposta1 not in ["menu", "interagir", "atacar", "continuar", "usar atributo" ]:
+    elif resposta1 not in ["menu", "interagir", "atacar", "continuar"]:
        print("\ncomando não identificado, tente novamente")
     resposta1 = input("O que quer fazer?\n")
 
@@ -191,10 +194,11 @@ while resposta1 not in "continuar":
       resposta1 = input("O que quer fazer?\n")
    elif resposta1 == "interagir":
        interaçoes = input("\ncom o que deseja interagir?\ngaveta\narmário\njanela\ncama\n\n") 
-       #usado para ver o que tem dentro da gaveta
+       #usado para ver o que tem dentro da cada interação
        if interaçoes == "gaveta":
          if verificar_quantidade(inventário, "pistola[2]") and verificar_quantidade(inventário, "faca[1]"):
             print("\nNão tem nada aqui(NÃO INSISTA!)\n")
+
          elif "pistola[2]" in inventário:
            print("você abre a gaveta, tem uma uma faca dentro.")
            yesnot = input("o que você quer pegar?\nfaca\n\nOBS: digite exatamente como escrito acima para que prossiga\n\n")
@@ -203,6 +207,7 @@ while resposta1 not in "continuar":
             adicionar_item(inventário, "faca[1]", 1)
             print(f"Agora você tem {inventário['faca[1]']}")
             resposta1 = input("O que quer fazer?\n")
+
          elif "faca[1]" in inventário:
              print("você abre a gaveta, tem uma pistola dentro.")
              yesnot = input("o que você quer pegar?\npistola\n\nOBS: digite exatamente como escrito acima para que prossiga\n\n")
@@ -227,7 +232,8 @@ while resposta1 not in "continuar":
                adicionar_item(inventário, "faca[1]", 1)
                print(f"Agora você tem {inventário['faca[1]']} faca\n")
                resposta1 = input("O que quer fazer?\n")
-       if interaçoes == "armário":
+
+       elif interaçoes == "armário":
          if interagir == 2:
             print("Sem mais interações por aqui\n")
             resposta1 = input("O que quer fazer?\n")
@@ -237,6 +243,7 @@ while resposta1 not in "continuar":
             if valor_aleatorio < 10:
                print("não tem nada no armário")
                interagir = 2
+               valor_aleatorio -= arcano
                resposta1 = input("O que quer fazer?\n")
             else:
                yesnot = input("Tem um cesto de roupas, quer dar uma olhada?\nsim\nnao\n")
@@ -245,18 +252,20 @@ while resposta1 not in "continuar":
                   if valor_aleatorio < 10:
                      print("tinha várias aranhas, elas pularam na sua cara, você se espantou e fechou o armário")
                      interagir = 2
+                     valor_aleatorio -= arcano
                      resposta1 = input("O que quer fazer?\n")
                   else:
                      print("Você achou roupas limpas!")
                      adicionar_item(inventário, "roupas_limpas[4]", 1)
                      interagir = 2
                      resposta1 = input("O que quer fazer?\n")
-                  
+
+       elif interaçoes not in ["gaveta", "armário", "janela", "cama"]:
+         print("\nComando não identificado")
+         resposta1 = input("O que quer fazer?\n")
    elif resposta1 == "atacar":
      print("você socou o ar, nada acontece\n")
 
-   elif resposta1 == "usar atributo":
-       print("nenhum atributo pode ser usado aqui\n")
 
    elif resposta1 == "comandos":
        print("Aqui estão todos os comandos possiveis:\ncontinuar\ninteragir\natacar\nmenu\nusar atributo\ncomandos\n")
@@ -267,8 +276,3 @@ while resposta1 not in "continuar":
 
 if resposta1 == "continuar":
     print("\nvocê saiu do quarto, você está em um corredor com várias portas\n")
-
-resposta1 = input("O que quer fazer?\n")
-if resposta1 == "atacar":
-   girarD20()
-   usar_atributo(valor_aleatorio)
