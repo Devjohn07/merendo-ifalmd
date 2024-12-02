d6ee149e20c488314d65fa9021a1693328191068
Devjohn07 <joaogamer6516@gmail.com>
2024-12-02 16:41:47 -0300

    Update clash.py

--- clash.py
+++ clash.py
@@ -1,14 +1,73 @@
+cartas = ["cavaleiro", "balão", "balao", "mago", "lenhador", "flechas", "mini pekka", "minipekka", "minepeka", "minepekka"]
+
+cartas2 = [ {
+    
+    "nome": "Mago",
+    "Tipo_de_Carta": "terrestre",
+    "Raridade": "RARA",
+    "Alvos": "terrestre-aéreo",
+    "Alcance": "5.5",
+    "Velocidade": "média",
+          
+},
+
+{
+    
+    "nome": "Cavaleiro",
+    "Tipo_de_Carta": "terrestre",
+    "Raridade": "COMUM",
+    "Alvos": "terrestre",
+    "Alcance": "perto",
+    "Velocidade": "média",
+          
+},
+
+{
+    
+    "nome": "Lenhador",
+    "Tipo_de_Carta": "terrestre",
+    "Raridade": "LENDÁRIA",
+    "Alvos": "terrestre",
+    "Alcance": "perto",
+    "Velocidade": "muito_rápida",
+          
+},
+
+{
+    
+    "nome": "Balão",
+    "Tipo_de_Carta": "aérea",
+    "Raridade": "ÉPICA",
+    "Alvos": "construções",
+    "Alcance": "perto",
+    "Velocidade": "média",
+          
+},
+
+{
+    
+    "nome": "Flechas",
+    "Tipo_de_Carta": "Feitiço",
+    "Raridade": "COMUM",
+    "Alvos": "terrestre-aéreo",
+    "Alcance": None,
+    "Raio": "1,4",
+          
+          }
+]
+
 resposta = input("Digite o nome da carta que deseja saber as informações, temos as seguintes cartas disponiveis:\n\nCavaleiro\nBalão\nMago\nLenhador\nFlechas\nMini pekka\n\n").lower().strip()
 
-while resposta not in ["cavaleiro", "balão", "balao", "mago", "lenhador", "flechas", "mini pekka", "minipekka"]:
+while resposta not in cartas:
     print("Resposta inválida, tente novamente\n")
     resposta = input("Digite a carta desejada: ").lower().strip()
 
 continuar = "sim"
 while continuar not in ["nao", "não", "n"] :
     if resposta == "mago":
-        print("\n\tTipo de carta: TROPA TERRESTRE\n\tRaridade: RARA\n\tAlvos: terrestre/aéreo\n\tAlcance: 5.5\n\tVelocidade: média\n\n\nEstatisticas que mudam de acordo com o nivel:\n\t\tNIVEL 3\n\n\tPontos de vida: 356\n\tDano: 133\n\tDPS: 95\n\n\t\tNIVEL 15\n\n\tPontos de vida: 1100\n\tDano: 410\n\tDPS: 292\n")
-        continuar = input("Quer ver as informações de outra carta? ").lower().strip()
+        print(cartas2[0])
+
+        continuar = input("\nQuer ver as informações de outra carta? ").lower().strip()
         if continuar in ["sim", "s"]:
             resposta = input("Digite a carta desejada: ").lower().strip()
         elif continuar in ["nao", "n"]:
@@ -18,8 +77,11 @@ while continuar not in ["nao", "não", "n"] :
             resposta = input("Digite a carta desejada: ").lower().strip()
 
     if resposta == "cavaleiro":
-        print("\n\tTipo de carta: TROPA TERRESTRE\n\tRaridade: COMUM\n\tAlvos: terrestre\n\tAlcance: Perto\n\tVelocidade: média\n\n\nEstatisticas que mudam de acordo com o nivel:\n\t\tNIVEL 1\n\n\tPontos de vida: 690\n\tDano: 79\n\tDPS: 65\n\n\t\tNIVEL 15\n\n\tPontos de vida: 2560\n\tDano: 293\n\tDPS: 244\n")
-        continuar = input("Quer ver as informações de outra carta? ").lower().strip()
+        print("\n  nome-----Tipo--Raridade---Alvos-Alcance-Velocidade\n   | \t     |         | \t|     |     |")
+        for stat in cavaleiro:
+            print(stat["nome"], stat["Tipo_de_Carta"], stat["Raridade"], stat["Alvos"], stat["Alcance"], stat["Velocidade"], sep="/")
+
+        continuar = input("\nQuer ver as informações de outra carta? ").lower().strip()
         if continuar in ["sim", "s"]:
             resposta = input("Digite a carta desejada: ").lower().strip()
         elif continuar in ["nao", "n"]:
@@ -29,8 +91,11 @@ while continuar not in ["nao", "não", "n"] :
             resposta = input("Digite a carta desejada: ").lower().strip()
 
     if resposta in ["balão", "balao"]:
-        print("\n\tTipo de carta: TROPA AÉREA\n\tRaridade: ÉPICA\n\tAlvos: construções\n\tAlcance: Perto\n\tVelocidade: média\n\n\nEstatisticas que mudam de acordo com o nivel:\n\t\tNIVEL 6\n\n\tPontos de vida: 1050\n\tDano: 400\n\tDPS: 200\n\tDano de morte: 150\n\n\t\tNIVEL 15\n\n\tPontos de vida: 2446\n\tDano: 932\n\tDPS: 466\n\tDano de morte: 349\n")
-        continuar = input("Quer ver as informações de outra carta? ").lower().strip()
+        print("\n nome--Tipo--Raridade---Alvos-Alcance-Velocidade\n   | \t |    | \t  |     |       |")
+        for stat in balao:
+            print(stat["nome"], stat["Tipo_de_Carta"], stat["Raridade"], stat["Alvos"], stat["Alcance"], stat["Velocidade"], sep="/")
+
+        continuar = input("\nQuer ver as informações de outra carta? ").lower().strip()
         if continuar in ["sim", "s"]:
             resposta = input("Digite a carta desejada: ").lower().strip()
         elif continuar in ["nao", "n"]:
@@ -40,8 +105,11 @@ while continuar not in ["nao", "não", "n"] :
             resposta = input("Digite a carta desejada: ").lower().strip()
 
     if resposta == "lenhador":
-        print("\n\tTipo de carta: TROPA TERRESTRE\n\tRaridade: LENDÁRIA\n\tAlvos: terrestre\n\tAlcance: Perto\n\tVelocidade: muito rápida\n\n\nEstatisticas que mudam de acordo com o nivel:\n\t\tNIVEL 9\n\n\tPontos de vida: 1060\n\tDano: 200\n\tDPS: 250\n\n\t\tNIVEL 15\n\n\tPontos de vida: 1865\n\tDano: 352\n\tDPS: 440\n")
-        continuar = input("Quer ver as informações de outra carta? ").lower().strip()
+        print("\n  nome-----Tipo--Raridade---Alvos-Alcance-Velocidade\n   | \t     |         | \t|     |       |")
+        for stat in lenhador:
+            print(stat["nome"], stat["Tipo_de_Carta"], stat["Raridade"], stat["Alvos"], stat["Alcance"], stat["Velocidade"], sep="/")
+
+        continuar = input("\nQuer ver as informações de outra carta? ").lower().strip()
         if continuar in ["sim", "s"]:
             resposta = input("Digite a carta desejada: ").lower().strip()
         elif continuar in ["nao", "n"]:
@@ -51,8 +119,10 @@ while continuar not in ["nao", "não", "n"] :
             resposta = input("Digite a carta desejada: ").lower().strip()
 
     if resposta == "flechas":
-        print("\n\tTipo de carta: FEITIÇO\n\tRaridade: COMUM\n\tAlvos: terrestre/aéreo\n\tRaio: 1,4\n\n\nEstatisticas que mudam de acordo com o nivel:\n\t\tNIVEL 1\n\n\tDano: 144\n\tDano na torre: 36\n\n\t\tNIVEL 15\n\n\tDano: 534\n\tDano na torre: 135\n")
-        continuar = input("Quer ver as informações de outra carta? ").lower().strip()
+        print("\nnome-----Tipo-Raridade----Alvos-----Alcance-Velocidade\n | \t   | \t   | \t   | \t       |    |")
+        for stat in flechas:
+            print(stat["nome"], stat["Tipo_de_Carta"], stat["Raridade"], stat["Alvos"], stat["Alcance"], stat["Raio"], sep="/")
+        continuar = input("\nQuer ver as informações de outra carta? ").lower().strip()
         if continuar in ["sim", "s"]:
             resposta = input("Digite a carta desejada: ").lower().strip()
         elif continuar in ["nao", "n"]:
@@ -61,7 +131,7 @@ while continuar not in ["nao", "não", "n"] :
             print("Comando não identificado, tente novamente:\n")
             resposta = input("Digite a carta desejada: ").lower().strip()
 
-    if resposta in  ["mini pekka", "minipekka"]:
+    if resposta in  ["mini pekka", "minipekka", "minepekka", "minepeka"]:
         print("\n\tTipo de carta: TROPA TERRESTRE\n\tRaridade: RARA\n\tAlvos: terrestre\n\tAlcance: Perto\n\tVelocidade: rápida\n\n\nEstatisticas que mudam de acordo com o nivel:\n\t\tNIVEL 3\n\n\tPontos de vida: 642\n\tDano: 340\n\tDPS: 212\n\n\t\tNIVEL 15\n\n\tPontos de vida: 1983\n\tDano: 1050\n\tDPS: 656\n")
         continuar = input("Quer ver as informações de outra carta? ").lower().strip()
         if continuar in ["sim", "s"]:
